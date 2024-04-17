223da3671b3ccf40fec6c059f701021043a94017
Weston Fausett <a02276419+ece3700gitgenesys@usu.edu>
2024-04-17 16:15:44 -0600

    IDK - Refactor ps2_controller_TB.sv to improve test stimulus and add validation

--- testbench/peripheral/ps2_controller_TB.sv
+++ testbench/peripheral/ps2_controller_TB.sv
@@ -46,42 +46,133 @@ module ps2_controller_TB;
 
 
 // TODO fill in stimulus section 
-// CAn do initial block with delays 
+// Can do initial block with delays 
 // or always @ posedge clk
 // if you nest them there is sequential programming?
-// to test we emulate the wave form of the ps2 controller
-// as the reciver we sample of the fallign edge of the clock
-// very similar to UART except I have to add paraity bit (with XOR)
-    // set data signal 
-    // wait several clock cycles half ps2 period #40 (#80 is 1 ps2 clock period)
-    // do clock edge (clock will get low) (on falling edge of clock)
-    // alternate what changing
-        // data wait clk data wait clk ... ...
-        // data
-        // #40
-        // clk <= 0
-        // #40 
-        // clk <= 1
-        // #40
+// to test we emulate the waveform of the ps2 controller
+// as the receiver we sample on the falling edge of the clock
+// very similar to UART except I have to add parity bit (with XOR)
+// set data signal 
+// wait several clock cycles half ps2 period #40 (#80 is 1 ps2 clock period)
+// do clock edge (clock will get low) (on falling edge of clock)
+// alternate what changing
+// data wait clk data wait clk ... ...
+// data
+// #40
+// clk <= 0
+// #40 
+// clk <= 1
+// #40
+initial begin
+    while (rst);
+    #100;
+    @(posedge clk);
+    while (1) begin
+
+        // Always tri-state before each test
+        _en <= 'h0000;
+        @(posedge clk);
+
+        // Set mode
+        _mode = $random();
+        if (_mode) begin
+            // Set output
+            $write("clk=%4d    Testing output mode:\n", clk_count);
+            $fwrite(fid,"clk=%4d    Testing output mode:\n", clk_count);
+            axi_wr(ps2_controllerx_MODE, 'hFFFF, _err);
+            if (_err) begin
+                fail_count++;
+                $write("clk=%4d    FAILED! Error writing to mode register\n", clk_count);
+                $fwrite(fid,"clk=%4d    FAILED! Error writing to mode register\n", clk_count);
+            end
+            else begin
+                // TODO implement stimulus waveform
+                _data = $random();
+                #40;
+                clk <= 0;
+                #40;
+                clk <= 1;
+                #40;
+            end
+        end
+    end
+end
 
 
 
 
 // Parity bit is the XOR of the 8 data bits
 // 1 is odd parity
-// have special parity register that is a sigle bit
-// everytime we send a databit intialize paritybit to 1
+// have special parity register that is a single bit
+// everytime we send a databit initialize paritybit to 1
 // then for each bit we send we XOR the parity bit with the data bit
-// once we've gone through all bit the parity bit will be correct
+// once we've gone through all bits the parity bit will be correct
 // not with start or stop bit on the data bits
 // [start, 8 data, parity, stop] total bits is 11 bit only 8 are data
 
+logic _parity; // Parity bit
+
+initial begin
+    _parity = 1; // Initialize parity bit to 1
+
+    // Send start bit
+    _data = 0;
+    #40;
+    clk <= 0;
+    #40;
+    clk <= 1;
+    #40;
+
+    // Send data bits
+    for (int i = 0; i < 8; i++) begin
+        _data = $random();
+        _parity ^= _data; // XOR parity bit with data bit
+        #40;
+        clk <= 0;
+        #40;
+        clk <= 1;
+        #40;
+    end
+
+    // Send parity bit
+    _data = _parity;
+    #40;
+    clk <= 0;
+    #40;
+    clk <= 1;
+    #40;
+
+    // Send stop bit
+    _data = 1;
+    #40;
+    clk <= 0;
+    #40;
+    clk <= 1;
+    #40;
+end
 
 
-// at the end the valid bt should go high
-// have the testbench automatically check itself 
 
-// incremet fail pass counters to then be able to see how many fail and pass
+
+logic _valid; // Valid bit
+
+// Initialize fail and pass counters
+int fail_count = 0;
+int pass_count = 0;
+
+// At the end, the valid bit should go high
+always @(posedge clk) begin
+    if (_valid) begin
+        pass_count++;
+        $write("clk=%4d    PASSED! Valid bit is high\n", clk_count);
+        $fwrite(fid,"clk=%4d    PASSED! Valid bit is high\n", clk_count);
+    end
+    else begin
+        fail_count++;
+        $write("clk=%4d    FAILED! Valid bit is low\n", clk_count);
+        $fwrite(fid,"clk=%4d    FAILED! Valid bit is low\n", clk_count);
+    end
+end
 
 
 
