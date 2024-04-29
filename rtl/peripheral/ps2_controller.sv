c06632980aa7bba3774ffa8ddf2a0eb16b27647f
Weston Fausett <a02276419+ece3700gitgenesys@usu.edu>
2024-04-29 11:23:13 -0600

    Add PS/2 controller module with clock edge detector

--- rtl/peripheral/ps2_controller.sv
+++ rtl/peripheral/ps2_controller.sv
@@ -1,3 +1,17 @@
+module ps2_controller #(
+    // Add any necessary parameters here
+) (
+    input logic clk,          // System clock
+    input logic rst_n,        // Reset (active-low)
+
+    input logic ps2_clk,      // PS/2 clock input
+    input logic ps2_data,     // PS/2 data input
+
+    // Outputs
+    output data[7:0] data,          // Data received from PS/2 controller
+    output logic valid,             // Asserted when data is valid
+    output logic err                // Asserted when error is detected
+);
 // Edge detector
 // needs to be on ps2 clk
 // have register (ps2_clk_previous)
@@ -5,6 +19,9 @@
 // if ps2_clk_previous is high and current is low (( if ps2_clk_previous & !ps2_clk))
 
 // assign ps2_clk_neg_edge = ps2_clk_previous & !ps2_clk;
+    // Edge detector for PS/2 clock
+    logic ps2_clk_previous;
+    assign ps2_clk_neg_edge = ps2_clk_previous & !ps2_clk;
 
 // need an always statement to assign ps2_clk_previous to ps2_clk
 // have if 
@@ -13,3 +30,18 @@
 // this gives neg edge to tell when to sample bit
 // clk process is sensitive to core clock
 // if ps2_negede do statemachine
+    always_ff @(posedge clk) begin
+        if (!rst_n) begin
+            ps2_clk_previous <= 1'b0;
+        end
+        else begin
+            ps2_clk_previous <= ps2_clk;
+        end
+    end
+
+    // PS/2 controller logic
+    // TODO: Implement PS/2 controller functionality here
+
+
+endmodule
+
